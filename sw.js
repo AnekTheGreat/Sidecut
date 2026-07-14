d7ed20e269356c86fcc13cfec1394916df0ebaf4
AnekTheGreat <166177289+AnekTheGreat@users.noreply.github.com>
2026-07-14 18:18:45 -0400

    Update CACHE_NAME to version 32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sidecut-shell-v31.75';
+const CACHE_NAME = 'sidecut-shell-v32';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(
