0541ba1bc6c673513d05843f1e72856b2c1fb54f
AnekTheGreat <166177289+AnekTheGreat@users.noreply.github.com>
2026-07-17 19:03:17 -0400

    Update CACHE_NAME to sidecut-shell-v34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sidecut-shell-v33.9';
+const CACHE_NAME = 'sidecut-shell-v34';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
